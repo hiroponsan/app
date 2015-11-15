2eb2c69d36fa06abb5a4ce0ebb5af6752d566067
hiroponsan <Chuo-univTanaka@outlook.jp>
2015-11-15 17:28:23 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
    before_filter :configure_permitted_parameters, if: :devise_controller?
+   
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nickname, :email, :depart, :password, ) }
