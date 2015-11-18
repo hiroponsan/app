ad73d7031420493465ce6bbf53785ed3e93a23fd
hiroponsan <Chuo-univTanaka@outlook.jp>
2015-11-18 18:19:45 +0900

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :show
 
   def show
     @product = Product.find(params[:id])
