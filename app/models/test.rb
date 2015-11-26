0c3faffc4c08236aa7883f5d8be9b29513752246
hiroponsan <Chuo-univTanaka@outlook.jp>
2015-11-26 20:02:40 +0900

    commit

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,13 @@
 class Test < ActiveRecord::Base
-  has_attached_file :image, :styles => { :large => "400x400", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :large => "600x600", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :product
   belongs_to :user
 
    validates :year, :semester, :textbook, :image, presence: true
+
+
 
 
 end
